064349a400fbca1c9e3a70bd9c3e12e6ab8cce64
prabdeep singh <prabdeepsingh062@gmail.com>
2025-01-22 15:30:18 +0530

    updated

--- index.js
+++ index.js
@@ -37,6 +37,7 @@ const { resolve } = require('path');
 
 const app = express();
 const port = 3010;
+app.use(express.json());
 
 app.use(express.static('static'));
 
@@ -44,8 +45,15 @@ app.get('/', (req, res) => {
   res.sendFile(resolve(__dirname, 'pages/index.html'));
 });
 
+app.post('/students/above-threshold', (req, res) => {
+  const { threshold } = req.body;
+  const students = require('./data.json').filter((student) => student.total > threshold);
+  if (students.length === 0) {
+    return res.json({ count: 0, students: [] });
+  }
+  return res.json({ count: students.length, students: students.map(({ name, total }) => ({ name, total })) });
+});
+
 app.listen(port, () => {
   console.log(`Example app listening at http://localhost:${port}`);
 });
-
-
